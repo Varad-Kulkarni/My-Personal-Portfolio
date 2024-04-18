039953f52fa58c2b63805202d847cff253779258
Varad Kulkarni <kulkarnivarad110@gmail.com>
2024-04-18 22:29:50 +0530

    Added changes of sideline in about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,73 +1,81 @@
 import React from 'react'
 
+import './About.css'
+
 const About = () => {
   return (
     <div>
       <div className="experience">
-        <h1>Experience</h1>
-        <ul>
-          <li><h3>Software Engineer at Siemens</h3>
-            <ul>
-              <li>Completed 3+ features end-to-end for inplace project.</li>
-              <li>Given release presentations about new features to customers with completion of all pre-release activities</li>
-              <li>Given demo presentations about tasks/features to architects/managers.</li>
-              <li>Documented all new changes for reference of customers.</li>
-              <li>Completed some tasks with optimal algorithms/logic approved by architect</li>
-            </ul>
-          </li>
-        </ul>
+        <h1 className="h1-heading">Experience</h1>
+        <div className='left-line'>
+          <ul>
+            <li><h3>Software Engineer at Siemens</h3>
+              <ul>
+                <li>Completed 3+ features end-to-end for inplace project.</li>
+                <li>Given release presentations about new features to customers with completion of all pre-release activities</li>
+                <li>Given demo presentations about tasks/features to architects/managers.</li>
+                <li>Documented all new changes for reference of customers.</li>
+                <li>Completed some tasks with optimal algorithms/logic approved by architect</li>
+              </ul>
+            </li>
+          </ul>
 
-        <ul>
-          <li><h3>Software Engineer at Pratiti Technologies</h3>
-            <ul>
-              <li>Working at client-side (Siemens) on project.</li>
-              <li>Done training on C#, C++ and web development when I have graduate engineer trainee here</li>
-              <li>Completed daily assignments given in the training</li>
-              <li>Completed project (library management system) as given after training and implement suggestions after review of project.</li>
-            </ul>
-          </li>
-        </ul>
+          <ul>
+            <li><h3>Software Engineer at Pratiti Technologies</h3>
+              <ul>
+                <li>Working at client-side (Siemens) on project.</li>
+                <li>Done training on C#, C++ and web development when I have graduate engineer trainee here</li>
+                <li>Completed daily assignments given in the training</li>
+                <li>Completed project (library management system) as given after training and implement suggestions after review of project.</li>
+              </ul>
+            </li>
+          </ul>
 
-        <ul>
-          <li><h3>Intern React developer at Shardings</h3>
-            <ul>
-              <li>Created react native currency converter application.</li>
-              <li>Attending daily meetings.</li>
-              <li>Using google cloud platform pull application repository and push changes.</li>
-            </ul>
-          </li>
-        </ul>
+          <ul>
+            <li><h3>Intern React developer at Shardings</h3>
+              <ul>
+                <li>Created react native currency converter application.</li>
+                <li>Attending daily meetings.</li>
+                <li>Using google cloud platform pull application repository and push changes.</li>
+              </ul>
+            </li>
+          </ul>
 
-        <ul>
-          <li><h3>Intern Java developer at VDerive</h3>
-            <ul>
-              <li>Manually test the application and fixed the bugs in application.</li>
-              <li>Created UML diagram of application.</li>
-              <li>Created testing document for application</li>
-              <li>Created a java class to handle input/output in CSV file using given methods.</li>
-            </ul>
-          </li>
-        </ul>
+          <ul>
+            <li><h3>Intern Java developer at VDerive</h3>
+              <ul>
+                <li>Manually test the application and fixed the bugs in application.</li>
+                <li>Created UML diagram of application.</li>
+                <li>Created testing document for application</li>
+                <li>Created a java class to handle input/output in CSV file using given methods.</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <div className="education">
-        <h1>Education</h1>
-        <ul>
-          <li>B. Tech in computer science and engineering (2019 – 2023) Government college of engineering, Aurangabad (CGPA = 8.1)</li>
-          <li>High school education in science (2017 – 2019) Kulbhushan Junior College, Aurangabad.(12th percentage = 70.3%) (MHT-CET percentile = 96.96%)</li>
-          <li>School education in Pioneers secondary school with 10th percentage of 90.8%.</li>
-        </ul>
+        <h1 className="h1-heading">Education</h1>
+        <div className="left-line">
+          <ul>
+            <li>B. Tech in computer science and engineering (2019 – 2023) Government college of engineering, Aurangabad (CGPA = 8.1)</li>
+            <li>High school education in science (2017 – 2019) Kulbhushan Junior College, Aurangabad.(12th percentage = 70.3%) (MHT-CET percentile = 96.96%)</li>
+            <li>School education in Pioneers secondary school with 10th percentage of 90.8%.</li>
+          </ul>
+        </div>
       </div>
 
       <div className="achievements">
-        <h1>Achievements</h1>
-        <ul>
-          <li>Solved 29+ contests on codechef and have reached 3 stars on codechef. (codechef profile)</li>
-          <li>Had 1600+ contest rating on leetcode. (leetcode profile)</li>
-          <li>Handled leader works as mini project and main project group leader</li>
-          <li>Have 5 stars in JAVA and 4 stars in MYSQL in hackerrank. (hackerrank profile)</li>
-          <li>I have qualified GATE exam in first attempt in 3rd year</li>
-        </ul>
+        <h1 className="h1-heading">Achievements</h1>
+        <div className="left-line">
+          <ul>
+            <li>Solved 29+ contests on codechef and have reached 3 stars on codechef. (codechef profile)</li>
+            <li>Had 1600+ contest rating on leetcode. (leetcode profile)</li>
+            <li>Handled leader works as mini project and main project group leader</li>
+            <li>Have 5 stars in JAVA and 4 stars in MYSQL in hackerrank. (hackerrank profile)</li>
+            <li>I have qualified GATE exam in first attempt in 3rd year</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
